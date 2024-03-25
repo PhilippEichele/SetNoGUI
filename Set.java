eaca0df2ff56b89c3fdc2db1459c430d923817ab
Eichele, Philipp <philipp.eichele@students.hs-mainz.de>
2024-03-25 14:46:20 +0100

    added checkForSet method

--- Set.java
+++ Set.java
@@ -1,11 +1,30 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Set {
     public static void main(String[] args){
         Deck deck = new Deck();
         deck.printDeck();
 
-        System.out.println();
+    }
 
-        deck.shuffle();
-        deck.printDeck();
+    public static boolean checkForSet(Card c1, Card c2, Card c3){
+        HashSet<Colour> colours = new HashSet<>(Arrays.asList(c1.getColour(), c2.getColour(), c3.getColour()));
+        if (colours.size() == 2){
+            return false;
+        }
+        HashSet<Shape> shapes = new HashSet<>(Arrays.asList(c1.getShape(), c2.getShape(), c3.getShape()));
+        if(shapes.size() == 2){
+            return false;
+        }
+        HashSet<Shading> shadings = new HashSet<>(Arrays.asList(c1.getShading(), c2.getShading(), c3.getShading()));
+        if(shadings.size() == 2){
+            return false;
+        }
+        HashSet<Number> numbers = new HashSet<>(Arrays.asList(c1.getNumber(), c2.getNumber(), c3.getNumber()));
+        if(numbers.size() == 2){
+            return false;
+        }
+        return true;
     }
 }
